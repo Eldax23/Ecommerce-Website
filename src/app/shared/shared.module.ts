0b5304f107259162c753a42ce36a41083e99a606
Eldax <mohamedabu.el3ela@gmail.com>
2024-10-31 23:10:57 +0200

    Organizing the structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { SelectComponent } from './components/select/select.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SelectComponent],
   imports: [CommonModule, RouterModule, HttpClientModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, SelectComponent],
 })
 export class SharedModule {}
